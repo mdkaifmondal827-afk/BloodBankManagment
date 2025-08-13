f38ae9f439cd2c644fbde6aa7cdeca91e1a3a22c
mdkaifmondal827-afk <mdkaifmondal827@gmail.com>
2025-08-13 08:18:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// Hamburger menu toggle
 
-// Hamburger menu toggle
 document.getElementById("menu-toggle").addEventListener("click", function() {
     document.getElementById("nav-links").classList.toggle("active");
 });
-
-
-
-// Example functions for buttons
 function registerDonor() {
   alert("Redirecting to Donor Registration Page...");
 }
@@ -15,10 +9,9 @@ function registerDonor() {
 function requestBlood() {
   alert("Redirecting to Blood Request Page...");
 }
-
-// Example counters (dynamic update possible from backend/localStorage)
 document.getElementById("donorsCount").innerText = 120;
 document.getElementById("bloodUnits").innerText = 350;
 document.getElementById("livesSaved").innerText = 200;
+
 
 
